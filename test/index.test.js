3b0c435e115f7fc9323e4c56b6f27137df7ae326
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2021-11-09 22:49:58 +0100

    ADD test

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+describe('League Tests', function() {
+    it('is server launched', function(done) {
+  
+      if (5 == 5) {
+        done();
+      } else {
+        done(new Error("Not sure what's happened."));
+      }
+  
+    });
+  
+  });
